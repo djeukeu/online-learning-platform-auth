73432fe1d56b226fe5c974b9ac445259fe3ca52e
djeukeu <dmxchristian@gmail.com>
2025-07-15 20:07:59 +0100

    feat: implement the authorize request for route security

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 
 import { getAllUserController, getUserController } from 'src/controller/user';
+import authorizeRequest from 'src/middleware/authorizeRequest';
 import { getUserSchema } from 'src/schema/user';
 
 const userRouter = Router();
 
-userRouter.get('/', getAllUserController);
+userRouter.get('/', authorizeRequest, getAllUserController);
 
-userRouter.get('/:id', getUserSchema, getUserController);
+userRouter.get('/:id', authorizeRequest, getUserSchema, getUserController);
 
 export default userRouter;
