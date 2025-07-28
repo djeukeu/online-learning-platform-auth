4d0b1b0c7994dbe09dd904dea2c5eba9197292e8
djeukeu <dmxchristian@gmail.com>
2025-07-28 13:04:42 +0100

    db migration

--- prisma/migrations/20250719182150_development/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `status` on the `user` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `user` DROP COLUMN `status`,
-    ADD COLUMN `role` ENUM('ADMIN', 'STUDENT', 'INSTRUCTOR') NOT NULL DEFAULT 'STUDENT';
